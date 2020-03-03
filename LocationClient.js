7a12d351df4b9ecc31554503d434ddea1614d2a0
Andres Medina <amedina@pivotal.io>
2020-03-03 10:24:49 -0800

    default back to current position if last location is not known

--- LocationClient.js
+++ LocationClient.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import * as Constants from "expo-constants";
 
 // this function is un-tested as it is fairly prescriptive
 // consider adding tests if actual logic is added into this function
@@ -13,5 +14,16 @@ export async function getLocation() {
     }
 
     // location does not need high accuracy - last position is *fine*
-    return Location.getLastKnownPositionAsync();
+    try {
+        return await Location.getLastKnownPositionAsync();
+    } catch {
+        // fall back to current position if there is no known last position
+        // `enableHighAccuracy` is needed for Android Emulator
+        // https://github.com/expo/expo/issues/5504#issuecomment-526630788 ¯\_(ツ)_/¯
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+            return Location.getCurrentPositionAsync( { enableHighAccuracy: true });
+        } else {
+            return Location.getCurrentPositionAsync({ });
+        }
+    }
 }
